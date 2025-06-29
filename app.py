f05a42f8dd7b928b1e58b86b4a5e68f3ed446622
Vishal-M1205 <vishalmanivannan1205@gmail.com>
2025-06-29 14:05:23 +0530

    File added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template_string
+import os
 
 app = Flask(__name__)
 
@@ -106,4 +107,5 @@ def google_form():
     """)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local dev
+    app.run(host='0.0.0.0', port=port)
